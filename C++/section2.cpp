c483118f59c8c992b8d9924405b4822d0911e62e
Adrian Dina <bogdanadrian.dina@gmail.com>
2024-12-29 15:59:51 +0200

    update

--- C++/section2.cpp
+++ C++/section2.cpp
@@ -119,3 +119,86 @@ int main()
     }
 }
 
+
+
+
+/* 
+Store the Board Using the State Enum
+0   ⛰️   0   0   0   0
+0   ⛰️   0   0   0   0
+0   ⛰️   0   0   0   0
+0   ⛰️   0   0   0   0
+0   0    0   0  ⛰️   0
+*/
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+using std::cout;
+using std::ifstream;
+using std::istringstream;
+using std::string;
+using std::vector;
+
+enum class State {kEmpty, kObstacle};
+
+// TODO: Change the return type to use auto or
+// explicitly indicate vector<State>
+vector<State> ParseLine(string line) {
+    istringstream sline(line);
+    int n;
+    char c;
+    // Change the variable type for row to be a vector<State>
+    vector<State> row;
+    while (sline >> n >> c && c == ',') {
+        // Push back State::kEmpty if n is 0, and State::kObstacle otherwise
+        row.push_back(n == 0 ? State::kEmpty : State::kObstacle);
+    }
+    return row;
+}
+
+// TODO: Modify the return type here as well. Just
+// as above, the board will contain State objects
+// instead of ints.
+vector<vector<State>> ReadBoardFile(string path) {
+    ifstream myfile(path);
+    // Modify the board declaration to store State objects
+    vector<vector<State>> board{};
+    if (myfile) {
+        string line;
+        while (getline(myfile, line)) {
+            // Parse each line into a vector<State>
+            vector<State> row = ParseLine(line);
+            board.push_back(row);
+        }
+    }
+    return board;
+}
+
+
+// TODO: Modify the function signature below to accept
+// cells with State type instead of int type.
+string CellString(State cell) {
+    switch(cell) {
+        case State::kObstacle: return "⛰️   ";
+        default: return "0   "; 
+    }
+}
+
+void PrintBoard(const vector<vector<State>> board) {
+    for (int i = 0; i < board.size(); i++) {
+        for (int j = 0; j < board[i].size(); j++) {
+            // Call CellString on each element of the board before streaming to cout
+            cout << CellString(board[i][j]);
+        }
+        cout << "\n";
+    }
+}
+
+int main() {
+  auto board = ReadBoardFile("1.board");
+  PrintBoard(board);
+}
+
