649e227a6d80f13daed7d0362022a935b88e2392
Adrian Dina <bogdanadrian.dina@gmail.com>
2024-12-31 18:06:30 +0200

    update

---
+++ Rust/projects/struct/structs/target/debug/deps/structs-bde99470d1d34ee4.d
@@ -0,0 +1,5 @@
+/Users/adriandina/repos/Udacity/rust/projects/struct/structs/target/debug/deps/structs-bde99470d1d34ee4: src/main.rs
+
+/Users/adriandina/repos/Udacity/rust/projects/struct/structs/target/debug/deps/structs-bde99470d1d34ee4.d: src/main.rs
+
+src/main.rs:
--- rust/projects/struct/structs/target/debug/structs.d
+++ rust/projects/struct/structs/target/debug/structs.d
@@ -1,1 +1,1 @@
-/Users/bogdandina/repos/rust/projects/struct/structs/target/debug/structs: /Users/bogdandina/repos/rust/projects/struct/structs/src/main.rs
+/Users/adriandina/repos/Udacity/rust/projects/struct/structs/target/debug/structs: /Users/adriandina/repos/Udacity/rust/projects/struct/structs/src/main.rs
